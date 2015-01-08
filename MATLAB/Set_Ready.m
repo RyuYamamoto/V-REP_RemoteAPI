9c21c1aabb427f320d891694fad322da814c282b
RyuYamamoto <rottertarmenation.y11.r18@gmail.com>
2015-01-08 10:05:44 +0900

    File upload

--- MATLAB/Set_Ready.m
+++ MATLAB/Set_Ready.m
@@ -1,11 +1,7 @@
 ﻿function Set_Ready()
     load('data/ServoID');
     
-<<<<<<< HEAD
     %下半身
-=======
-    %Set Ready Position
->>>>>>> aa4e37387606921f527262b2977053f2e9cd0a06
     ReadyPos = [0 0 20 ; 0 0 20];
     AngPos = Cal_Inv_Kine(ReadyPos);
     Servo_OutPut(AngPos,0.05);
