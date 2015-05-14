6d9224cac1c62aa2750d00b7125e18a28cc7e383
RyuYamamoto <rottertarmenation.y11.r18@gmail.com>
2015-05-14 15:47:38 +0900

    Add sample program

---
+++ test/sample_head_motion.m
@@ -0,0 +1,33 @@
+close all;clc;
+clear all
+disp('Sample Program');
+vrep=remApi('remoteApi');
+vrep.simxFinish(-1);
+clientID=vrep.simxStart('127.0.0.1',19999,true,true,5000,5);
+pause(1);
+
+try
+	if(clietID>-1)
+		disp('Connected to remote API server');
+
+		%joint of handle('' is please according to the name of your robot joint)
+		[res1,pan]=vrep.simxGetObjectHandle(clientID,'pan joint name',vrep.simx_opmode_oneshot_wait);
+        [res2,tilt]=vrep.simxGetObjectHandle(clientID,'tilt joint name',vrep.simx_opmode_oneshot_wait);
+
+		%Move Joint(angle_pan and angle_tilt are angle to move the joint)
+		vrep.simxPauseCommunication(clientID,1);
+		[err1]=vrep.simxSetJointTargetPosition(clientID,HipR_JointA0,-target_value,vrep.simx_opmode_oneshot);
+        [err2]=vrep.simxSetJointTargetPosition(clientID,KneeR_JointA0,-target_value,vrep.simx_opmode_oneshot);
+		vrep.simxPauseCommunication(clientID,0);
+		pause(1);
+
+		disp('End.');
+	else
+		disp('Failed connecting to remote API server');
+	end 
+	vrep.delete();
+catch err
+	vrep.simxFinish(clientID);
+	vrep.delete();
+end;
+disp('Program ended');
