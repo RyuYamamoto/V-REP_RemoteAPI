aef2f5254b47ec660563d69ac3c5d3ccc472eb92
RyuYamamoto <rottertarmenation.y11.r18@gmail.com>
2015-05-14 17:28:10 +0900

    fix bug

--- test/sample_head_motion.m
+++ test/sample_head_motion.m
@@ -9,15 +9,19 @@ pause(1);
 try
 	if(clietID>-1)
 		disp('Connected to remote API server');
-
+		
+		%Set Angle Value(degree to radian)
+		angle_pan = 45 * 3.14 / 180;
+		angle_tilt = 45 * 3.14 / 180;
+		
 		%joint of handle('' is please according to the name of your robot joint)
 		[res1,pan]=vrep.simxGetObjectHandle(clientID,'pan joint name',vrep.simx_opmode_oneshot_wait);
         	[res2,tilt]=vrep.simxGetObjectHandle(clientID,'tilt joint name',vrep.simx_opmode_oneshot_wait);
 
 		%Move Joint(angle_pan and angle_tilt are angle to move the joint)
 		vrep.simxPauseCommunication(clientID,1);
-		[err1]=vrep.simxSetJointTargetPosition(clientID,HipR_JointA0,-target_value,vrep.simx_opmode_oneshot);
-        	[err2]=vrep.simxSetJointTargetPosition(clientID,KneeR_JointA0,-target_value,vrep.simx_opmode_oneshot);
+		[err1]=vrep.simxSetJointTargetPosition(clientID,pan,angle_pan,vrep.simx_opmode_oneshot);
+        	[err2]=vrep.simxSetJointTargetPosition(clientID,tilt,angle_tilt,vrep.simx_opmode_oneshot);
 		vrep.simxPauseCommunication(clientID,0);
 		pause(1);
 
